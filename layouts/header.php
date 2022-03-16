ea155ae6c1bc6c1eb24abf07147a95d6043bb014
monzur.mysoftheaven@gmail.com <onlymafizur@gmail.com>
2022-03-16 18:53:41 +0600

    view crypto list.

---
+++ layouts/header.php
@@ -0,0 +1,16 @@
+<?php 
+    include 'config.php';
+?>
+
+<Doctype html>
+    <head>
+        <title>Test</title>
+        <!-- CSS only -->
+        <!-- https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href='https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css' />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" integrity="sha512-10/jx2EXwxxWqCLX/hHth/vu2KY3jCF70dCQB8TSgNjbCVAC/8vai53GfMDrO2Emgwccf2pJqxct9ehpzG+MTw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href='assets/style.css' />
+    </head>
+    <body class="container">
+
---
+++ list.php
@@ -0,0 +1,62 @@
+<?php
+
+include('layouts/header.php');
+
+$sql = "SELECT Name,Symbol,price,rank,marketCap,priceChangePercentage24h,totalSupply,ytdPriceChangePercentage
+        FROM test_cryptos_info";
+$result = $conn->query($sql);
+$table_content = '';
+
+if(!empty($result)){
+    if($result->num_rows > 0){
+        while($row = $result->fetch_assoc()){
+            $Name = $row['Name'];
+            $table_content .= "
+                <tr>
+                    <td>".$row['Name']."</td>
+                    <td>".$row['Symbol']."</td>
+                    <td>".$row['rank']."</td>
+                    <td>".$row['price']."</td>
+                    <td>".$row['marketCap']."</td>
+                    <td>".$row['priceChangePercentage24h']."</td>
+                    <td>".$row['totalSupply']."</td>
+                    <td>".$row['ytdPriceChangePercentage']."</td>
+                    <td><a href='view.php?id=".$Name."' class='btn btn-sm btn-success'><i class='fa fa-eye'><i> View</a></td>
+                </tr>
+            ";
+        }
+    }
+    else{
+        $table_content .= 'No data found.';
+    }
+}
+else{
+    $table_content .= 'No data found.';
+}
+
+?>
+
+<div class="container-fluid">
+    <div class="row table-responsive">
+        <table id="cryptoTable" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Symbol</th>
+                    <th>Rank</th>
+                    <th>Price</th>
+                    <th>Marketcap</th>
+                    <th>Price Change Percentage 24h</th>
+                    <th>Total Supply</th>
+                    <th>Ytd Price Change Percentage</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?= $table_content ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include('layouts/footer.php'); ?>
